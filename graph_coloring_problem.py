3891f4e5d9fadd285943513d9709f4a6531d6e46
Alba Fernández Coronado <alba.coronado.2002@gmail.com>
2024-11-07 14:25:15 +0100

    changes

--- graph_coloring_problem.py
+++ graph_coloring_problem.py
@@ -5,19 +5,6 @@ import time
 
 ############# GENETIC ALGORITHM FUNCTIONS #############
 
-# Create adjacent matrix of the map
-def create_adjacent_matrix(df):
-    nodes = len(df)
-    adjacency_matrix = np.zeros((nodes, nodes), dtype=int)
-
-    # Vectorized intersection checks using GeoPandas/NumPy
-    for i in range(nodes):
-        adjacency_matrix[i] = df.geometry.intersects(df.geometry[i]).astype(int)
-        adjacency_matrix[i, i] = 0  # Remove self-adjacency
-
-    return adjacency_matrix
-
-
 # Define the fitness function to compute the penalty
 def fitness_function(matrix, individuals):
     # Directly sum conflicts for adjacent nodes
@@ -45,6 +32,7 @@ def two_point_crossover(parent1, parent2):
     child2 = np.concatenate((parent2[:point1], parent1[point1:point2], parent2[point2:]))
     return child1, child2
 
+
 # Uniform crossover function
 def uniform_crossover(parent1, parent2, crossover_prob=0.5):
     n = len(parent1)
@@ -61,6 +49,7 @@ def uniform_crossover(parent1, parent2, crossover_prob=0.5):
 
     return child1, child2
 
+
 def crossover(crossover_type, parent1, parent2, prob):
     if crossover_type == 'uniform':
         child1, child2 = uniform_crossover(parent1, parent2, prob)
@@ -71,118 +60,127 @@ def crossover(crossover_type, parent1, parent2, prob):
     return child1, child2
 
 
-
 # Tournament selection function
-def tournament_selection(population, matrix, tournament_size=2):
-    new_population = []
+def tournament_selection(population_dict, tournament_size=2):
+    selected_population_dict = []
     for _ in range(tournament_size):
-        random.shuffle(population)
-        for i in range(0, len(population)-1, 2):
-            ind1 = population[i]
-            ind2 = population[i+1]
-            inds_to_compete = np.array([ind1, ind2])
+        random.shuffle(population_dict)
+        for i in range(0, len(population_dict)-1, 2):
+            ind1 = population_dict[i]
+            ind2 = population_dict[i+1]
+            
+            best_ind = ind1 if ind1['penalty'] < ind2['penalty'] else ind2
+            selected_population_dict.append(best_ind)
 
-            fitness_vector = fitness_function(matrix, inds_to_compete)
-            best_ind = inds_to_compete[np.argmin(fitness_vector)]
-            new_population.append(best_ind)
-
-    return np.array(new_population)
+    return selected_population_dict
 
 
 # Mutation function
 def mutation(individuals, prob_mutation, num_colors):
     for individual in individuals:
-        if random.uniform(0, 1) <= prob_mutation:
-            position = random.randint(0, len(individual)-1)
-            individual[position] = random.randint(1, num_colors)
+        for i in individual:
+            if random.uniform(0, 1) <= prob_mutation:
+                individual[i] = random.randint(0, num_colors-1)
 
     return individuals
 
-def get_results(results, penalty, num_colors, generations, fitness_evaluations):
-    mean_penalty = np.sum(penalty) / len(penalty)
-    min_penalty = np.min(penalty)
 
-    sorted_penalty = np.sort(penalty)
-    n_low_5_percent = int(0.20 * len(penalty))
-    lowest_5_percent_values = sorted_penalty[:n_low_5_percent]
-    mean_low_5_percent = np.mean(lowest_5_percent_values)
+def get_results(results, population_dict, num_colors, generations, fitness_evaluations):
+    penalties = [ind['penalty'] for ind in population_dict]  # Accedemos directamente a la lista
+    mean_penalty = np.mean(penalties)
+    min_penalty = np.min(penalties)
 
-    new_row = {'Mean Penalty': mean_penalty, 'Min Penalty':min_penalty, 'Low 20% penalty': mean_low_5_percent, 
-               'Generation': generations, 'Number colors': num_colors, 'Fitness evaluations': fitness_evaluations}
-    results = results._append(new_row, ignore_index = True)
+    sorted_penalty = np.sort(penalties)
+    n_low_20_percent = int(0.20 * len(penalties))
+    lowest_20_percent_values = sorted_penalty[:n_low_20_percent]
+    mean_low_20_percent = np.mean(lowest_20_percent_values)
+
+    new_row = {
+        'Mean Penalty': mean_penalty,
+        'Min Penalty': min_penalty,
+        'Low 20% penalty': mean_low_20_percent,
+        'Generation': generations,
+        'Number colors': num_colors,
+        'Fitness evaluations': fitness_evaluations
+    }
+    
+    results = results._append(new_row, ignore_index=True)
     return results
 
 
 ###################################### MAIN PROGRAM  ######################################
 
-def obtain_colours(matrix, population_size = 100, prob_mutation = 0.2, max_generations = 200,
+def obtain_colours(matrix, population_size = 300, prob_mutation = 0.1, max_generations = 500,
                    crossover_type = 'uniform', crossover_prob = 0.5):
     num_colors = max(np.sum(matrix, axis=0))
     nodes_graph = matrix.shape[0]
     best_palette = None
     results = pd.DataFrame()
+    results2 = pd.DataFrame()
 
     start_time = time.time()
 
     total_generations = 0
     fitness_evaluations = 0
 
-
     while num_colors > 0:
-        # Initialize the population
-        population = np.random.randint(num_colors, size=(population_size, nodes_graph))
         generations = 0
 
+        # Initialize the population
+        population = np.random.randint(num_colors, size=(population_size, nodes_graph))
+        
         # Evaluate individuals
         penalty = fitness_function(matrix, population)
+        
+        # Create the dictionary with the individuals of the current population and their penalty
+        population_dict = [
+            {'individual': individual, 'penalty': penalty_value}
+            for individual, penalty_value in zip(population, penalty)
+        ]
 
-        while np.min(penalty) > 0 and generations < max_generations:
+        while min(ind['penalty'] for ind in population_dict) > 0 and generations < max_generations:
 
             # Tournament selection to choose the individuals for reproduction
-            individuals_to_reproduce = tournament_selection(population, matrix)
+            individuals_to_reproduce_dict = tournament_selection(population_dict)
 
             # Crossover individuals to obtain the offspring
             new_population = []
-            for i in range(0, len(individuals_to_reproduce)-1, 2):
-                child1, child2 = crossover(crossover_type, individuals_to_reproduce[i], individuals_to_reproduce[i+1], crossover_prob)
+            for i in range(0, len(individuals_to_reproduce_dict)-1, 2):
+                child1, child2 = crossover(crossover_type, individuals_to_reproduce_dict[i]['individual'], individuals_to_reproduce_dict[i+1]['individual'], crossover_prob)
                 new_population.extend([child1, child2])
 
             # Apply Mutation to the new population
             new_population = mutation(new_population, prob_mutation, num_colors,)
-            population = new_population
 
-            # Evaluate the new population
-            penalty = fitness_function(matrix, population)
+            # Evaluate new population
+            penalty = fitness_function(matrix, new_population)
 
-            fitness_evaluations += len(penalty)
+            new_population_dict = [
+                {'individual': individual, 'penalty': penalty_value}
+                for individual, penalty_value in zip(new_population, penalty)
+            ]
 
-            results = get_results(results, penalty, num_colors, total_generations, fitness_evaluations)
+            # Update the population
+            population_dict = new_population_dict
 
+            fitness_evaluations += len(population_dict)
+            results2 = get_results(results2, population_dict, num_colors, total_generations, fitness_evaluations)
             generations += 1
             total_generations += 1
             
-            
         # If a valid solution is found, reduce the number of colors
-        if np.min(penalty) == 0:
-            num_colors -= 1
-            best_palette = population[np.argmin(penalty)]
-
+        if np.min([ind['penalty'] for ind in population_dict]) == 0:
+            best_palette = min(population_dict, key=lambda x: x['penalty'])['individual']
+            results = get_results(results, population_dict, num_colors, total_generations, fitness_evaluations)
             print(f"Number of colors: {len(np.unique(best_palette))}, Generation: {generations}")
+            num_colors -= 1
 
         else:
             break # No valid solution with fewer colors, so stop
 
     execution_time = time.time() - start_time
-
-    # Information for the report
-    """report_data = {
-        'num_colors': len(np.unique(best_palette)) if best_palette is not None else None,
-        'generations': generations,
-        'fitness_calls': fitness_call_counter[0],
-        'execution_time': execution_time
-    }"""
         
-    return best_palette, results, execution_time
+    return best_palette, results, results2, execution_time
 
 
 
